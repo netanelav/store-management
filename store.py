63c73d930745b751002d07b3717aa4de1125d383
Netanel <netanel4747@gmail.com>
2019-07-26 11:40:45 +0300

    implemetation of add/edit product and  get product by id methods

--- store.py
+++ store.py
@@ -58,6 +58,18 @@ def get_products():
         return json.dumps({'error': f'error with the db: {e}'})
 
 
+@get('/product/<id:int>')
+def get_product(id):
+    try:
+        with connection.cursor() as cursor:
+            sql = f"select * from products where id = '{id}'"
+            cursor.execute(sql)
+            product = cursor.fetchall()
+            return json.dumps({"STATUS": "SUCCESS", "PRODUCT": product, "CODE": 200})
+    except Exception as e:
+        return json.dumps({'error': f'error with the db: {e}'})
+
+
 @get("/category/<id:int>/products")
 def products_by_category(id):
     try:
@@ -67,6 +79,36 @@ def products_by_category(id):
             return json.dumps({"PRODUCTS": cursor.fetchall()})
     except Exception as e:
         return json.dumps({'error': f'error with the db: {e}'})
+
+
+@post('/product')
+def add_or_edit_product():
+    id = request.forms.get('id')
+    category = request.forms.get('category')
+    title = request.forms.get('title')
+    desc = request.forms.get('desc')
+    favorite = request.forms.get('favorite')
+    price = request.forms.get('price')
+    img_url = request.forms.get('img_url')
+    favorite = 1 if favorite == "on" else 0
+    try:
+        for i in [category, title, desc, favorite, price, img_url]:
+            if i == "" or i is None:
+                return json.dumps({"STATUS": "ERROR", "MSG": "Some parameters are missing", "CODE": 400})
+        with connection.cursor() as cursor:
+            products = json.loads(get_products())
+            for prod in products['PRODUCTS']:
+                if title != prod["title"]:
+                    sql = f"insert into products (title, category, description, favorite, price, img_url) VALUES ('{title}', '{category}', '{desc}', '{favorite}', '{price}', '{img_url}')"
+                else:
+                    prod_id = prod["id"]
+                    sql = f"update products set title = '{title}', category = '{category}', description = '{desc}', favorite = '{favorite}', price = '{price}', img_url = '{img_url}' where id = '{prod_id}'"
+                    break
+            cursor.execute(sql)
+            connection.commit()
+            return json.dumps({"STATUS": "SUCCESS", "MSG": "Product was added/updated successfully", "PRODUCT_ID": cursor.lastrowid, "CODE": 201})
+    except Exception as e:
+        return json.dumps({'error': f'could not add/edit product: {e}'})
 
 
 @get("/admin")
